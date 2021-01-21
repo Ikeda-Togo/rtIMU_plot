a3deb1675e3127fa250621a0fe6b5f3914b45105
Ikeda-Togo <syuugo151122@gmail.com>
2021-01-21 13:36:02 +0900

    update

--- Platinum_data/plot_data.py
+++ Platinum_data/plot_data.py
@@ -1,11 +1,32 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-df = pd.read_csv('step_up.csv', names=['num1', 'num2','num3','num4'])
+# フォントサイズ
+plt.rcParams["font.size"] = 30
 
-plt.figure()
-plt.plot(df['num1'],df['num2'],label='acc_angle')
-plt.plot(df['num1'],df['num3'],label= "gyro_angle")
-plt.plot(df['num1'],df['num4'],label= "filter_angle")
+# file_name = 'down_step'
+file_name = 'step_up'
+df = pd.read_csv(file_name+'.csv', names=['num1', 'num2','num3','num4'])
+
+fig=plt.figure()
+plt.plot(df['num1'],df['num2'],label='acc_pitch_angle')
+# plt.plot(df['num1'],df['num3'],label= "gyro_angle")
+plt.plot(df['num1'],df['num4'],label= "pitch_angle", linewidth = 3)
 plt.legend()
+fig.text(0.88, 0.06, '[s]')
+fig.text(0.05, 0.87, '[deg]')
+
+# Show the major grid lines with dark grey lines
+plt.grid(b=True, which='major', color='#666666', linestyle='-')
+
+# Show the minor grid lines with very faint and almost transparent grey lines
+plt.minorticks_on()
+plt.grid(b=True, which='minor', color='#999999', linestyle='-', alpha=0.2)
+
+
+
+manager = plt.get_current_fig_manager()
+manager.window.showMaximized()
+
 plt.show()
+fig.savefig(file_name+'_graph.png')
