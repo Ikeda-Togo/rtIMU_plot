580d0d2507ff819bffc78b322e6bdfdafd136edd
IkedaTogo <syuugo151122@gmail.com>
2022-01-11 19:59:16 +0900

    udpate

--- pico_read/servo_test2.py
+++ pico_read/servo_test2.py
@@ -1,13 +1,41 @@
 from machine import PWM, Pin
+import math
 import time
 
-servo1 = PWM(Pin(28))
+class Robot:
+    def __init__(self, body, arm):
+        self.body=body
+        self.arm=arm
+    def imu_deg_estimator(self, arm_deg):
+        b = self.arm
+        c = self.body
+        A = 180-arm_deg
+
+        a = math.sqrt((-math.cos(math.radians(A))*2*b*c)+(b*b)+(c*c))
+        print(-math.cos(math.radians(150)))
+        B =  math.degrees(math.asin(math.sin(math.radians(A))*b/a))
+
+        return B, a
+
+
+robot = Robot(128,107)
+servo1 = PWM(Pin(27)) #Left
+servo2 = PWM(Pin(28)) #Right
 servo1.freq(50)
+servo2.freq(50)
+servo1.duty_u16(4750)
+servo2.duty_u16(4950)
 
-max_duty = 65025
-dig_0 = 0.0725    #0°
-dig_90 = 0.12     #90°
+try:
+    while True :
+        deg = int(input("角度入力："))
+        print(robot.imu_deg_estimator(deg))
+        servo1.duty_u16(4750+deg*35)
+        servo2.duty_u16(4950-deg*35)
+        time.sleep(2)
 
-while True:
-    servo1.duty_u16(int(max_duty*dig_0))
-    servo1.duty_u16(int(max_duty*dig_90))
+
+except KeyboardInterrupt:
+    servo1.deinit()
+    servo2.deinit()
+    print("stop!!")
